4b3d9a6154db7de79a31d2880c7ae3993d7f2ad5
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-08-18 11:00:00 +0530

    number of distinct islands

--- 03_Graphs/01_Introduction_to_graphs/02_Number_of_distinct_islands.py
+++ 03_Graphs/01_Introduction_to_graphs/02_Number_of_distinct_islands.py
@@ -1,3 +1,31 @@
+"""
+Number of Islands
+Problem Statement: Given a grid of size NxM (N is the number of rows and M is the number of columns in the grid) consisting of ‘0’s (Water) and ‘1’s(Land). Find the number of islands.
+
+Note: An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically or diagonally i.e., in all 8 directions.
+
+Pre-req: Connected Components, Graph traversal techniques
+
+Examples:
+
+Example 1:
+
+Input:
+
+
+Output: 3
+
+Explanation:
+
+
+There are 3 islands as the different components are surrounded by water (i.e. 0), and there is no land connectivity in either of the 8 directions hence separating them into 3 islands.
+
+Example 2:
+
+Input:
+
+
+"""
 def number_of_islands(grid):
     r = len(grid)
     c = len(grid[0])
@@ -19,7 +47,7 @@ def dfs(i, j, grid, visited, directions):
         if nr >=0 and nc >=0 and nr < len(grid) and nc < len(grid[0]):
             if visited[nr][nc] ==0 and grid[nr][nc] ==1:
                 dfs(nr, nc, grid, visited, directions)
-grid =[]
+grid =[[]]
 number_of_islands(grid)
 
                 
