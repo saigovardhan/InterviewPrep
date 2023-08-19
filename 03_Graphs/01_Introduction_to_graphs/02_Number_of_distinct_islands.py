185ae881a479ae6b4a359775c8b560073c924775
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-08-19 11:00:00 +0530

    number of distinct islands

--- 03_Graphs/01_Introduction_to_graphs/02_Number_of_distinct_islands.py
+++ 03_Graphs/01_Introduction_to_graphs/02_Number_of_distinct_islands.py
@@ -19,7 +19,7 @@ def dfs(i, j, grid, visited, directions):
         if nr >=0 and nc >=0 and nr < len(grid) and nc < len(grid[0]):
             if visited[nr][nc] ==0 and grid[nr][nc] ==1:
                 dfs(nr, nc, grid, visited, directions)
-
+grid =[]
 number_of_islands(grid)
 
                 
