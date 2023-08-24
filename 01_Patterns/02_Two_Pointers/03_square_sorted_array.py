68cc3d6e8d2d22c0348422f9818a47eb811d578f
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-08-24 10:26:40 +0530

    square_sorted_array

--- 01_Patterns/02_Two_Pointers/03_square_sorted_array.py
+++ 01_Patterns/02_Two_Pointers/03_square_sorted_array.py
@@ -13,3 +13,21 @@ Example 2:
 Input: [-3, -1, 0, 1, 2]
 Output: [0 1 1 4 9]
 """
+from collections import deque
+def squaring_sorted_array(nums):
+    p1, p2 = 0, len(nums)-1
+    sol = deque()
+    while p1 <= p2:
+        p1_sqr = nums[p1]*nums[p1]
+        p2_sqr = nums[p2]*nums[p2]
+
+        if p1_sqr>=p2_sqr:
+            sol.appendleft(p1_sqr)
+            p1+=1
+        else:
+            sol.appendleft(p2_sqr)
+            p2-=1
+    
+    print(sol)
+
+squaring_sorted_array([-2, -1, 0, 2, 3])
