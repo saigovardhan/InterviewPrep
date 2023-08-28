06fd3bd7033414fc1911b786ae055a8ba1847020
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-08-28 11:42:13 +0530

    tabulation done

--- 02_Dynamic_Programming/01_1D_DP/02_frog_jump.py
+++ 02_Dynamic_Programming/01_1D_DP/02_frog_jump.py
@@ -1,19 +1,44 @@
 def frog_jump(nums):
     ind = len(nums)-1
-    sol = memoization(ind, nums)
+    dp = [float("inf") for _ in range(ind+2)]
+    # sol = memoization(ind, nums, dp)
+    sol = tabulation(ind, nums, dp)
     print(sol)
 
-def memoization(ind, nums):
+def memoization(ind, nums, dp):
     if ind ==0: return 0
 
-    if ind <0 : return 1e9
+    if ind <0 : return float("inf")
 
-    one_jump = memoization(ind-1, nums) + abs(nums[ind]-nums[ind-1])
+    if dp[ind] !=float("inf") : return dp[ind]
 
-    two_jump = 1e9
+    one_jump = memoization(ind-1, nums, dp) + abs(nums[ind]-nums[ind-1])
+
+    two_jump = float("inf")
     if ind >1:
-        two_jump = memoization(ind-2, nums)+ abs(nums[ind]-nums[ind-2])
+        two_jump = memoization(ind-2, nums, dp)+ abs(nums[ind]-nums[ind-2])
 
-    return min(one_jump, two_jump)
+    dp[ind] = min(one_jump, two_jump)
+    return dp[ind]
+
+def tabulation(ind, nums, dp):
+
+    dp[0] = 0
+
+    for ind in range(1, len(nums)):
+        one_jump = dp[ind-1] + abs(nums[ind]-nums[ind-1])
+        two_jump = float("inf")
+        if ind >1:
+            two_jump = dp[ind-2] + abs(nums[ind]-nums[ind-2])
+        dp[ind] = min(one_jump, two_jump)
+    print(dp)
+    return dp[len(nums)-1]
+        
+
+
+
+
+
 
 frog_jump([10 , 20, 30, 10])
+frog_jump([10, 50, 10])
---
+++ 02_Dynamic_Programming/01_1D_DP/03_frog_jump_k_steps.py
@@ -0,0 +1,2 @@
+def frog_jump_k_steps(nums, k):
+    
