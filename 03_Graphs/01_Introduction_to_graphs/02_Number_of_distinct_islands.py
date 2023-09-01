1b25e1ff50a8771a11b3e552102f4ea867d8ae4a
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-09-01 11:00:00 +0530

    number of distinct islands

--- 03_Graphs/01_Introduction_to_graphs/02_Number_of_distinct_islands.py
+++ 03_Graphs/01_Introduction_to_graphs/02_Number_of_distinct_islands.py
@@ -3,5 +3,9 @@ def number_of_islands(grid):
     c = len(grid[0])
     visited = [[0 for _ in range(c)] for _ in range(r)]
     directions = [[-1, 0], [0, 1], [0, -1], [1, 0]]
-    
+    islands = 0
+    for i in range(r):
+        for j in range(c):
+            if grid[i][j] == 1 and visited[i][j] ==0:
+                
 
