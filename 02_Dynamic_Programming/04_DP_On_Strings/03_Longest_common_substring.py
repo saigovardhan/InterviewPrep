90b5e1c1af3add94b0c1fd55e3177435dfde86a8
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-09-04 16:54:16 +0530

    longest common substring

--- 02_Dynamic_Programming/04_DP_On_Strings/03_Longest_common_substring.py
+++ 02_Dynamic_Programming/04_DP_On_Strings/03_Longest_common_substring.py
@@ -1,23 +1,25 @@
 def longest_common_substring(s1, s2):
     i, j = len(s1), len(s2)
-    sol = memoization(i, j, s1, s2, 0)
+    dp = [[0 for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
+
+    sol = tabulation( s1, s2, 0, dp)
+    print(dp)
     print(sol)
 
-
-def memoization(i, j, s1, s2, count):
-    if i == 0 or j == 0:
-        return count
-
-    if i < 0 or j < 0:
-        return 0
-
-    if s1[i-1] == s2[j-1]:
-        return memoization(i-1, j-1, s1, s2, count+1)
-
-    else:
-        top = memoization(i-1, j, s1, s2, 0)
-        left = memoization(i, j-1, s1, s2, 0)
-        return max(count, max(left, top))
+def tabulation(s1, s2, count, dp):
+    sol = ""
+    count = 0
+    for i in range(1, len(s1)+1):
+        for j in range(1, len(s2)+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = 1+dp[i-1][j-1]
+                count = max(dp[i][j], count)
+                sol+=s1[i-1]
+            else:
+                dp[i][j] = 0
+    
+    return count
 
 
-longest_common_substring("abceghijk", "abcdghija")
+
+longest_common_substring("abcdfhijkltuh", "abceghijkluyt")
