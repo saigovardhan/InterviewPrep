c40def269f5cf4c85b24db1cad4c4a89e8e94f47
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-09-04 17:47:14 +0530

    minimum insertions to make a string palindrome

--- 02_Dynamic_Programming/04_DP_On_Strings/05_Minimum_insertions_to_make_string_palindrome.py
+++ 02_Dynamic_Programming/04_DP_On_Strings/05_Minimum_insertions_to_make_string_palindrome.py
@@ -29,8 +29,33 @@ Output: 5
 Explanation: Inserting 5 characters the string becomes "leetcodocteel".
 
 
-###########
-reverse s
-find longest common subsequence
-n-longest common subsequence
+
 """
+
+
+def minimum_insertions_to_make_string_palindrome(s):
+    p = s[len(s)::-1]
+    dp = [[-1 for _ in range(len(s)+1)] for _ in range(len(p)+1)]
+    count = 0
+    sol = tabulation(s, p, dp, count)
+
+    print(len(s)-sol)
+
+
+def tabulation(s, p, dp, count):
+    for i in range(len(s)+1):
+        dp[0][i] = 0
+        dp[i][0] = 0
+
+    for i in range(1, len(s)+1):
+        for j in range(1, len(p)+1):
+            if s[i-1] == p[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+                count = max(count, dp[i][j])
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+    return count
+
+minimum_insertions_to_make_string_palindrome("mbadm")
+minimum_insertions_to_make_string_palindrome("leetcode")
