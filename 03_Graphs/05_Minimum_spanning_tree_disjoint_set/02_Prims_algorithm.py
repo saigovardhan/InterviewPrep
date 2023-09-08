35bf6787512039bb4a2d0345c2e7b4159df67951
saigovardhan <veggalam.saigovardhan@gmail.com>
2023-09-08 21:25:51 +0530

    graphs started

--- 03_Graphs/05_Minimum_spanning_tree_disjoint_set/02_Prims_algorithm.py
+++ 03_Graphs/05_Minimum_spanning_tree_disjoint_set/02_Prims_algorithm.py
@@ -1,5 +1,43 @@
 """
 prims algorithms 
 
-todo 
+adjList = { {0, 1, 2}, {0, 2, 1}, {1, 2, 1}, {2, 3, 2}, {3, 4, 1}, {4, 2, 2}}
 """
+from heapq import heappush, heappop
+
+def prims_algorithm(edges):
+    nodes = len(edges)
+    adjList = [[] for i in range(len(edges))]
+    seen = [0 for _ in range(nodes+1)]
+    minHeap = []
+    for parent, child, wt in edges:
+        adjList[parent].append([wt, child])
+    
+    heappush(minHeap, [0, 0])
+    sol = 0
+    while minHeap:
+        pwt, pnode = heappop(minHeap)
+        if seen[pnode] ==1:continue
+        seen[pnode] = 1
+        sol+=pwt
+        for wt,child in adjList[pnode]:
+            heappush(minHeap,[wt, child])
+
+    print(sol)
+
+prims_algorithm([
+    [0, 1, 2],
+    [0, 2, 1],
+    [1, 2, 1],
+    [2, 3, 2],
+    [3, 4, 1],
+    [4, 2, 2]
+])
+            
+
+        
+
+
+    
+
+
